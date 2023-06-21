571bf5f02207f564e459ef7592fbc56adcbfd524
HAMID MEER <85035495+Hamii-king-06@users.noreply.github.com>
2023-06-21 19:39:31 +0500

    Update HBF.py

--- HBF.py
+++ HBF.py
@@ -1,24 +1,13 @@
 import os, platform
-
 print(50*"-")
-
 print("[>>] JOIN MY WHATSAPP GROUP")
-
 print(50*"-")
-
 os.system('xdg-open https://chat.whatsapp.com/GrxWJLqOb7vCwUv765CCfX')
-
 import requests
-
 bit = platform.architecture()[0]
-
 if bit == '64bit':
-
-    os.system("python3 run64.py") 
-
-
+    from run642 import infinty
+    infinty()
 elif bit == '32bit':
-
     from run32 import infinty
-
     infinty()
